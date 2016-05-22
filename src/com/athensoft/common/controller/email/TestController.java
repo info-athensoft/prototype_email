b6e2afa4b2907c70a2d162224c9c215c3e9084c8
Athens Zhang <athens314@hotmail.com>
2016-05-22 17:19:21 -0400

    added test

---
+++ src/com/athensoft/common/controller/email/TestController.java
@@ -0,0 +1,13 @@
+package com.athensoft.common.controller.email;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/test.do")
+	public String goTest(){
+		return "test";
+	}
+}
